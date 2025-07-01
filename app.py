bef15358a2b9890e7a942586e9d965bb0ea31522
parthagni22 <parthpagnihotri@gmail.com>
2025-07-01 13:30:02 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, request, render_template
+from AggieCourses import *
+from pinecone import Pinecone
+
+application = Flask(__name__)
+
+app = application
+
+llm = AggieeRag()
+
+## route for home
+
+##import asyncio
+
+@app.route('/')
+def index():
+    return render_template("AggieCourses.html")
+
+@app.route('/llmtrigger', methods=['POST', 'GET'])
+def reports():
+    if request.method == 'POST':
+        if 'message' in request.form:
+            query = request.form['message']
+            prompt =llm.augment_prompt(query=query)
+            prompt = HumanMessage(
+            content=prompt
+            )
+
+            llm.messages.append(prompt)
+
+            res = llm.chatbot(llm.messages)
+            response= {"message":res.content}
+            print("#############----Output----##########################")
+            return(response)
+
+
+
+
+if __name__ == "__main__":
+    #app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run()
